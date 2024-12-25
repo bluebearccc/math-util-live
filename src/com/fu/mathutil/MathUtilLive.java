ba08652af179440ad81f3449423a3bfbcee62305
bluebearccc <ngaidang53@gmail.com>
2024-12-25 22:42:31 +0700

    Lan thu 2 up code

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -18,9 +18,14 @@ public class MathUtilLive {
         long result = MathUtil.getFactorial(5); //hàm tính đi coi là mấy
 
         System.out.println("expected: 5! = 120; actual: " + result);
-        
+
         System.out.println("expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
         //System.out.println("expected: 0! = 1; actual: " + MathUtil.getFactorial(0));
+
+        //THÊM CODE SAU LẦN ĐẦU ÚP CODE LÊN SERVER 10:40PM12/12/2024
+        
+        System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
 
         //MathUtil.getFactorial(-5); ném ngoại lệ thật rồi, ai biểu đưa cà chớn chi!!!
         //Kĩ thuật kiểm thử phần mềm: ước lượng xem giá trị trả về của hàm là gì
